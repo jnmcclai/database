e3ebe40078331949fdaeb336d3ef61bb3c88fbbe
jnmcclai <jonathan.mcclain@adtran.com>
2016-02-16 17:17:00 -0600

    added some more to db module

--- database.py
+++ database.py
@@ -16,7 +16,6 @@ class database():
                 ip: the database server IP address
                 port: the database server port (3306)
                 database: the database name
-                table: a database table name
                 username: database username credential
                 password: database password credential
             }
@@ -25,33 +24,43 @@ class database():
     Ideally requires external file containing hashed credentials
     """
 
-    def __init__(self, ip, port, database, table, username, pwd):
+    def __init__(self, ip, port, database, username, pwd):
         """
         Class initialization
         """
         self.ip = ip
         self.port = port
         self.database = database
-        self.table = table
+        self.username = username
+        self.pwd = pwd
 
     def db_connect(self):
         """
         Connect to a database
         """
-        self.db = MySQLdb.connect(host=self.ip, port=self.port, user=self.username, db=self.database)
+        self.db = MySQLdb.connect(host=self.ip, user=self.username, passwd=self.pwd, db=self.database)
         self.cur = self.db.cursor()
 
-        self.cur.execute()
 
     def db_push(self):
         """
         Push data to a MySQL database
         """
 
-    def db_pull(self):
+    def db_pull(self, sql_index=None, table=None):
         """
         Pull data from a MySQL database
+
+        args = {
+                    sql_index: SQL command
+                    table: database table to retrieve from
+                }
         """
+        #attempting an example
+        self.cur.execute("SELECT * FROM {0} WHERE 1".format(table))
+        for row in self.cur.fetchall():
+            print row[1]
+
     def db_truncate(self):
         """
         Truncate data from a MySQL database
@@ -69,7 +78,12 @@ if __name__ == '__main__':
     #initialize some example variables
     db_ip = '10.21.1.181'
     db_port = 3306
-    db_database = 'pqGeneral'
-    db_table = 'pqGeneral'
+    db_database = 'bsmEoxLoadTestBed'
+    db_table = 'temperature'
     db_username = 'pqgen'
     db_pwd = 'pqgen'
+
+    #initiate an instance
+    instance = database(db_ip, db_port, db_database, db_username, db_pwd)
+    instance.db_connect()
+    instance.db_pull(table=db_table)
